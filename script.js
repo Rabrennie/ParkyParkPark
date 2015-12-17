0907e73ba19cf1648852953f8595c2b4b1d41d7a
Rab Rennie <RENNIERAB@gmail.com>
2015-12-17 22:12:13 +0000

    Moved javascript to own file

--- script.js
+++ script.js
@@ -1,134 +1,165 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var player,cursors, spriteMaterial;
-var speed = 300;
-var once = true;
+var renderer, stage, container, graphics, zoom,
+            world, boxShape, boxBody, planeBody, planeShape,chassisBody,player, cars=[];
 
-function Car(x, y, startSpeed, rotation, friction, car, material) {
-    this.car = game.add.sprite(x, y, car);
-    this.car.scale.setTo(2.5,2.5)
-    this.startSpeed = startSpeed;
-    this.speed= startSpeed;
-    game.physics.p2.enable(this.car)
-    this.friction = friction;
-    this.car.body.angularDamping = 0.9;
-    this.car.body.damping = 0.5;
-    this.car.body.mass = 0.1;
-    this.car.body.rotation = rotation;
-    this.car.body.onBeginContact.add(blockHit.bind(this), this.car);
-    this.collision = false;
-    this.car.body.setMaterial(material)
-    this.car.body.moveForward(this.startSpeed)
+        var carTexture;
 
-    this.moveForward = function(){
-        if (this.speed >= 25) {
-            this.speed = this.speed * this.friction;
-        } else if (this.speed >= 5){
-            this.speed = this.speed * (this.friction-0.005);
+        var PLAYER=Math.pow(2,1),
+            CAR=Math.pow(2,2);
+
+        init();
+        
+
+        function Car(x,y,w,h,angle,velX,velY,mass,world,container,collisionGroup,stage){
+            this.chassisBody = new p2.Body({
+                position: [x,y],
+                mass: mass,
+                angle: angle,
+                velocity: [velX, velY],
+            });
             
-        } else {
-            this.speed = 0;
+            this.boxShape = new p2.Box({ width: w, height: h });
+            this.boxShape.collisionGroup = collisionGroup;
+            this.boxShape.collisionMask = PLAYER | CAR
+            this.chassisBody.addShape(this.boxShape);
+            world.addBody(this.chassisBody);
+            // Create the vehicle
+            this.vehicle = new p2.TopDownVehicle(this.chassisBody);
+            // Add one front wheel and one back wheel - we don't actually need four :)
+
+            this.chassisBody.frontWheel = this.vehicle.addWheel({
+                localPosition: [0, 0.5] // front
+            });
+
+            this.chassisBody.frontWheel.setSideFriction(200);
+            // Back wheel
+            this.chassisBody.backWheel = this.vehicle.addWheel({
+                localPosition: [0, -0.5] // back
+            });
+            this.chassisBody.backWheel.setSideFriction(200); // Less side friction on back wheel makes it easier to drift
+            this.vehicle.addToWorld(world);
+
+            this.graphics = new PIXI.Graphics();
+            this.graphics.beginFill(0xff0000);
+            this.texture = carTexture;
+            this.sprite = new PIXI.Sprite(this.texture);
+            this.graphics.addChild(this.sprite);
+            this.graphics.drawRect(-this.boxShape.width/2, -this.boxShape.height/2, this.boxShape.width, this.boxShape.height);
+            this.sprite.width = -this.boxShape.width;
+            this.sprite.height = -this.boxShape.height;
+            this.sprite.position={x:-this.boxShape.width/2, y:this.boxShape.height/2}
+            this.sprite.scale.x = -this.sprite.scale.x
+            // Add the box to our container
+            container.addChild(this.graphics);
+
+            this.update = function(){
+                this.graphics.position.x = this.chassisBody.position[0];
+                this.graphics.position.y = this.chassisBody.position[1];
+                this.graphics.rotation =   this.chassisBody.angle;
+            }
         }
-        this.car.body.moveForward(this.speed);
-    }
-    this.rotateLeft = function(){
-    
-        this.car.body.rotateLeft(this.speed/this.startSpeed*100);
-    }
-    this.rotateRight = function(){
-        
-        this.car.body.rotateRight(this.speed/this.startSpeed*100);
-    }
-    this.setZeroRotation = function(){
-        this.car.body.setZeroRotation();
-    }
-}
 
-function preload() {
-  
-  game.load.image('car', 'assets/car.png');
-  game.load.image('background', 'assets/background.png');
+        function init(){
 
-}
+            // Init p2.js
+            // Create a dynamic body for the chassis
+            world = new p2.World({
+                gravity : [0,0]
+            });
 
-function create() {
-    game.physics.startSystem(Phaser.Physics.P2JS);
-    this.car = game.add.sprite(0, 0, 'background');
+            zoom = 50;
 
-    spriteMaterial = game.physics.p2.createMaterial('spriteMaterial');
+            // Initialize the stage
+            renderer =  PIXI.autoDetectRenderer(800, 600),
+            stage = new PIXI.Stage(0xFFFFFF);
+            container = new PIXI.Container(),
+            PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+            PIXI.loader
+                        .add('car', 'assets/car.png')
+                        .load(function (loader, resources) {
+                            carTexture = resources.car.texture;
+                            player = new Car(0,0,0.5,1,-1.5,15,0,2,world,container,PLAYER,stage);
+                            animate();
+            });
+            stage.addChild(container);
+            document.body.appendChild(renderer.view);
+            // Add transform to the container
+            container.position.x =  renderer.width/10; // center at origin
+            container.position.y =  renderer.height/10;
+            container.scale.x =  zoom;  // zoom in
+            container.scale.y = -zoom; // Note: we flip the y axis to make "up" the physics "up"
+            
 
-    player = new Car(100,200,300,20,0.997,'car', spriteMaterial);
-    cursors = game.input.keyboard.createCursorKeys();
-    new Car(200,200,300,20,0.997,'car', spriteMaterial);
+            
 
-    //  4 trues = the 4 faces of the world in left, right, top, bottom order
-    game.physics.p2.setWorldMaterial(spriteMaterial, true, true, true, true);
+            world.on("impact",function(evt){
+                console.log(evt)
+                var bodyA = evt.bodyA,
+                bodyB = evt.bodyB;
+                bodyA.frontWheel.setSideFriction(2);
+                bodyB.frontWheel.setSideFriction(2);
 
-    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
-    //  those 2 materials collide it uses the following settings.
-    //  A single material can be used by as many different sprites as you like.
-    var contactMaterial = game.physics.p2.createContactMaterial(spriteMaterial, spriteMaterial);
+                bodyA.backWheel.setSideFriction(2);
+                bodyB.backWheel.setSideFriction(2);
+            });0
 
-    contactMaterial.friction = 0.3;     // Friction to use in the contact of these two materials.
-    contactMaterial.restitution = 0.5;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-    contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-    contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-    contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-    contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-    contactMaterial.surfaceVelocity = 0; 
 
-    // carru = game.add.sprite(200, 170, 'car');
-    // game.physics.p2.enable(carru);
-    // carru.body.angle = 200;
-    // carru.body.damping  = 0.5;
-    // carru.body.angularDamping  = 0.8;
-    
-}
+            
+            // Draw the box.
+            
 
-function update() {
-    if(!player.collision){
-        
-        if(player.speed == 0){
-            player = new Car(100,200,300,20,0.997,'car', spriteMaterial);
+            var keys = {
+                '37': 0, // left
+                '39': 0, // right
+                '38': 0, // up
+                '40': 0 // down
+            };
+            var maxSteer = 20000;
+            window.addEventListener("keydown",function (evt){
+                keys[evt.keyCode] = 1;
+                onInputChange();
+            });
+            window.addEventListener("keyup",function (evt){
+                keys[evt.keyCode] = 0;
+                onInputChange();
+            });
+            function onInputChange(){
+                // Steer value zero means straight forward. Positive is left and negative right.
+                player.chassisBody.frontWheel.steerValue = maxSteer * (keys[37] - keys[39]);
+                player.chassisBody.backWheel.setBrakeForce(0);
+                if(keys[40]){
+                    if(player.chassisBody.backWheel.getSpeed() > 0.1){
+                        // Moving forward - add some brake force to slow down
+                        player.chassisBody.backWheel.setBrakeForce(2);
+                    } else {
+                        // Moving backwards - reverse the engine force
+                        player.chassisBody.backWheel.setBrakeForce(2);
+                    }
+                }
+            }
         }
-    } else {
-        if(player.car.body.velocity.x<=0.5&&player.car.body.velocity.y<=0.5){
-            player = new Car(100,200,300,20,0.997,'car', spriteMaterial);
+
+        // Animation loop
+        function animate(t){
+            t = t || 0;
+            requestAnimationFrame(animate);
+
+            // Move physics bodies forward in time
+            world.step(1/60);
+
+            // Transfer positions of the physics objects to Pixi.js
+            
+            player.update();
+            for (var i = cars.length - 1; i >= 0; i--) {
+                cars[i].update()
+            };
+            // console.log(p2.vec2.length(player.chassisBody.velocity))
+            if(p2.vec2.length(player.chassisBody.velocity) <= 0.05){
+                player.chassisBody.backWheel.setBrakeForce(2);
+                player.boxShape.collisionGroup = collisionGroup;
+                cars.push(player);
+                player = new Car(0,0,0.5,1,-1.5,15,0,2,world,container,PLAYER,stage);
+                
+            }
+            // Render scene
+            renderer.render(stage);
         }
-        player.speed = Math.sqrt(player.car.body.velocity.x*player.car.body.velocity.x+player.car.body.velocity.y*player.car.body.velocity.y);
-    }
-    player.moveForward();
-    if (cursors.left.isDown) {player.rotateLeft()} 
-    else if (cursors.right.isDown){player.rotateRight()}
-    else {player.setZeroRotation()}
-
-    
-
-    
-}
-
-function blockHit (body, bodyB, shapeA, shapeB, equation) {
-
-    //  The block hit something.
-    //  
-    //  This callback is sent 5 arguments:
-    //  
-    //  The Phaser.Physics.P2.Body it is in contact with. *This might be null* if the Body was created directly in the p2 world.
-    //  The p2.Body this Body is in contact with.
-    //  The Shape from this body that caused the contact.
-    //  The Shape from the contact body.
-    //  The Contact Equation data array.
-    //  
-    //  The first argument may be null or not have a sprite property, such as when you hit the world bounds.
-
-    
-    this.collision = true;
-    if (body)
-    {
-        console.log(result = 'You last hit: ' + body.sprite.key);
-    }
-    else
-    {
-        console.log(result = 'You last hit: The wall :)');
-    }
-
-}
