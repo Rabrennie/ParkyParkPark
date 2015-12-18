0dc7d0338869dd0ef68b8104b235947a47301210
Rab Rennie <RENNIERAB@gmail.com>
2015-12-18 04:58:13 +0000

    added wheels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var renderer, stage, container, graphics, zoom,
             world, boxShape, boxBody, planeBody, planeShape,chassisBody,player, cars=[],wall=[];
 
-        var carTexture;
+        var carTexture,wheelTexture;
 
         var PLAYER=Math.pow(2,1),
             CAR=Math.pow(2,2),
@@ -10,7 +10,7 @@ var renderer, stage, container, graphics, zoom,
         init();
         
 
-        function Car(x,y,w,h,angle,velX,velY,mass,world,container,collisionGroup,stage){
+        function Car(x,y,w,h,angle,velX,velY,mass,world,container,collisionGroup,stage,texture){
             this.chassisBody = new p2.Body({
                 position: [x,y],
                 mass: mass,
@@ -41,14 +41,39 @@ var renderer, stage, container, graphics, zoom,
 
             this.graphics = new PIXI.Graphics();
             this.graphics.beginFill(0xff0000);
-            this.texture = carTexture;
-            this.sprite = new PIXI.Sprite(this.texture);
+            this.wheelSprite = [];
+
+            for (var i = 3; i >= 0; i--) {
+                this.wheelSprite[i] = new PIXI.Sprite(wheelTexture);
+                this.graphics.addChild(this.wheelSprite[i]);
+                this.wheelSprite[i].scale.x = 0.016
+                this.wheelSprite[i].scale.y = 0.016
+                this.wheelSprite[i].anchor = new PIXI.Point(1,0.5)
+
+            };
+            //FL
+            this.wheelSprite[0].position={x:-0.25, y:0.15}
+            
+            //FR
+            this.wheelSprite[1].position={x:0.4-this.wheelSprite[1].width, y:0.15}
+
+            //BL
+            this.wheelSprite[2].position={x:-0.25, y:-0.35}
+            //BR
+            this.wheelSprite[3].position={x:0.4-this.wheelSprite[3].width, y:-0.35}
+            this.sprite = new PIXI.Sprite(texture);
             this.graphics.addChild(this.sprite);
+            
             this.graphics.drawRect(-this.boxShape.width/2, -this.boxShape.height/2, this.boxShape.width, this.boxShape.height);
             this.sprite.width = -this.boxShape.width;
             this.sprite.height = -this.boxShape.height;
             this.sprite.position={x:-this.boxShape.width/2, y:this.boxShape.height/2}
             this.sprite.scale.x = -this.sprite.scale.x
+            
+
+            //wheel sprites
+            
+
             // Add the box to our container
             container.addChild(this.graphics);
 
@@ -95,26 +120,29 @@ var renderer, stage, container, graphics, zoom,
 
             // Initialize the stage
             renderer =  PIXI.autoDetectRenderer(800, 600),
-            stage = new PIXI.Stage(0xFFFFFF);
+            stage = new PIXI.Stage(0x282B2A);
+            renderer.backgroundColor = 0x282B2A;
             container = new PIXI.Container(),
             PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
             PIXI.loader
-                        .add('car', 'assets/car.png')
+                        .add('car', 'assets/car1.png')
+                        .add('wheel', 'assets/wheel.png')
                         .load(function (loader, resources) {
                             carTexture = resources.car.texture;
-                            wall[0] = new Wall(720/zoom,-240/zoom,20/zoom,600/zoom,0,container,world)
-                            wall[1] = new Wall(320/zoom,60/zoom,800/zoom,20/zoom,0,container,world)
-                            wall[2] = new Wall(320/zoom,-540/zoom,800/zoom,20/zoom,0,container,world)
-                            wall[4] = new Wall(-80/zoom,-240/zoom,20/zoom,600/zoom,0,container,world)
+                            wheelTexture = resources.wheel.texture;
+                            wall[0] = new Wall(800/zoom,-300/zoom,20/zoom,600/zoom,0,container,world)
+                            wall[1] = new Wall(400/zoom,0/zoom,800/zoom,20/zoom,0,container,world)
+                            wall[2] = new Wall(400/zoom,-600/zoom,800/zoom,20/zoom,0,container,world)
+                            wall[4] = new Wall(0/zoom,-300/zoom,20/zoom,600/zoom,0,container,world)
                             
-                            player = new Car(0,0,0.5,1,-1.5,15,0,2,world,container,PLAYER,stage);
+                            player = new Car(50/zoom,-30/zoom,0.5,0.875,-1.5708,15,0,2,world,container,PLAYER,stage,carTexture);
                             animate();
             });
             stage.addChild(container);
             document.body.appendChild(renderer.view);
             // Add transform to the container
-            container.position.x =  renderer.width/10; // center at origin
-            container.position.y =  renderer.height/10;
+            container.position.x =  0; // center at origin
+            container.position.y =  0;
             container.scale.x =  zoom;  // zoom in
             container.scale.y = -zoom; // Note: we flip the y axis to make "up" the physics "up"
             
@@ -127,13 +155,25 @@ var renderer, stage, container, graphics, zoom,
                 bodyB = evt.bodyB;
 
                 if(bodyA.shapes[0].collisionGroup == CAR || bodyA.shapes[0].collisionGroup == PLAYER) {
-                    bodyA.frontWheel.setSideFriction(2);
-                    bodyA.backWheel.setSideFriction(2);
+                    bodyA.frontWheel.setSideFriction(3);
+                    bodyA.backWheel.setSideFriction(3);
                 }
                 if(bodyB.shapes[0].collisionGroup == CAR || bodyB.shapes[0].collisionGroup == PLAYER) {
-                    bodyB.frontWheel.setSideFriction(2);
-                    bodyB.backWheel.setSideFriction(2);
+                    bodyB.frontWheel.setSideFriction(3);
+                    bodyB.backWheel.setSideFriction(3);
                 }
+
+                if(bodyB.shapes[0].collisionGroup == WALL && bodyA.shapes.collisionGroup == PLAYER) {
+                    window.setTimeout(function(){
+                    bodyA.frontWheel.setSideFriction(200);
+                    bodyA.backWheel.setSideFriction(200);}, 100)
+                }
+                if(bodyA.shapes[0].collisionGroup == WALL && bodyB.shapes.collisionGroup == PLAYER) {
+                    window.setTimeout(function(){
+                    bodyB.frontWheel.setSideFriction(200);
+                    bodyB.backWheel.setSideFriction(200);}, 100)
+                }
+
                 
             });0
 
@@ -160,6 +200,7 @@ var renderer, stage, container, graphics, zoom,
             function onInputChange(){
                 // Steer value zero means straight forward. Positive is left and negative right.
                 player.chassisBody.frontWheel.steerValue = maxSteer * (keys[37] - keys[39]);
+                player.wheelSprite[0].rotation = player.wheelSprite[1].rotation = 0.5*(keys[37] - keys[39])
                 player.chassisBody.backWheel.setBrakeForce(0);
                 if(keys[40]){
                     if(player.chassisBody.backWheel.getSpeed() > 0.1){
@@ -192,7 +233,7 @@ var renderer, stage, container, graphics, zoom,
                 player.chassisBody.backWheel.setBrakeForce(2);
                 player.boxShape.collisionGroup = CAR;
                 cars.push(player);
-                player = new Car(0,0,0.5,1,-1.5,15,0,2,world,container,PLAYER,stage);
+                player = new Car(50/zoom,-30/zoom,0.5,0.875,-1.5708,15,0,2,world,container,PLAYER,stage,carTexture);
                 
             }
             // Render scene
