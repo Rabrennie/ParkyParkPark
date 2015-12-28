c6718c98b0184947c5d40d845e2409a03264861e
Rab Rennie <RENNIERAB@gmail.com>
2015-12-28 22:18:58 +0000

    fix(input): down key now works

--- app/js/Input.js
+++ app/js/Input.js
@@ -12,7 +12,7 @@ export const keymap = {
   'left'      : '37',
   'right'     : '39',
   'up'        : '38',
-  'down'      : '30',
+  'down'      : '40',
   'enter'     : '13',
   'escape'    : '27',
   'backspace' : '8',
